0dcb5cdf5ffda54c7f55406187f736307487b8e7
sbarnham <barnham.s@etoncollege.org.uk>
2019-11-26 13:09:57 +0000

    success for most core tests most core methods implemented

--- Tamagotchi/Tamagotchi/Tamagotchi.swift
+++ Tamagotchi/Tamagotchi/Tamagotchi.swift
@@ -9,13 +9,74 @@
 import Foundation
 
 class Tamagotchi {
-    var health : Int = 10
-    var discipline : Int = 0
-    let name: String = ""
+    public var health : Int = 10
+    var discipline : Int = 100
+    var name: String = ""
     var age : Int = 0
-    var happiness : Int = 0
+    var happiness : Int = 100
     var weight : Int = 0
     var pooOnFloor : Int = 0
     var isIll : Bool = false
+    
+    func naming(input: String) {
+        name = input
+    }
+    
+    func die(age: Int, random: Int) -> Bool {
+        if age == 25 {
+            return true
+        } else if random > 998 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func poo(random: Int) {
+        if random < 200 {
+            pooOnFloor += 1
+        }
+    }
+    
+    func ill(random: Int, pooOnFloor: Int) {
+        if random > (800/(pooOnFloor + 1)) {
+            isIll = true
+        }
+    }
+    
+    func eatMeal() {
+        if health < 8 {
+            health += 1
+        }
+        weight += 3
+    }
+    
+    func eatSnack() {
+        if health < 5 {
+            health += 1
+        }
+        weight += 1
+    }
+    
+    func consumeMedicine() {
+        isIll = false
+    }
+    
+    func decreaseDiscipline(random: Int) {
+        if random < 400 {
+            discipline -= 25
+        } else if random < 50 {
+            discipline -= 75
+        }
+    }
+    
+    func disciplineTamagotchi() {
+        discipline += 25
+        happiness -= 25
+    }
+    
+    func sleep() {
+        age += 1
+    }
     
 }
--- Tamagotchi/TamagotchiTests/TamagotchiTest.swift
+++ Tamagotchi/TamagotchiTests/TamagotchiTest.swift
@@ -17,12 +17,68 @@ class TamagotchiTest: XCTestCase {
         //assert
         XCTAssertNotNil(tamagotchi)
         XCTAssertEqual(tamagotchi.health, 10)
-        XCTAssertEqual(tamagotchi.discipline, 0)
+        XCTAssertEqual(tamagotchi.discipline, 100)
         XCTAssertEqual(tamagotchi.name, "")
         XCTAssertEqual(tamagotchi.age, 0)
-        XCTAssertEqual(tamagotchi.happiness, 0)
+        XCTAssertEqual(tamagotchi.happiness, 100)
         XCTAssertEqual(tamagotchi.weight, 0)
         XCTAssertEqual(tamagotchi.pooOnFloor, 0)
         XCTAssertEqual(tamagotchi.isIll, false)
     }
-}
+    
+    func testTamagotchiDiesWhenAgeIs25() {
+        let tamagotchi = Tamagotchi()
+        let age = 25
+        let death = tamagotchi.die(age: age, random: 0)
+        XCTAssertEqual(death, true)
+    }
+    
+    func testTamagotchiPoosWhenRandonNumberBelow200() {
+        let tamagotchi = Tamagotchi()
+        let random = 199
+        tamagotchi.poo(random: random)
+        let expected = 1
+        XCTAssertEqual(tamagotchi.pooOnFloor, 1)
+    }
+    func testTamagotchiBecomesIllWhenRandonNumberAbove800DividedByPooOnFloor() {
+        let tamagotchi = Tamagotchi()
+        let random = 700
+        let randomForPoo = 199
+        tamagotchi.poo(random: randomForPoo)
+        tamagotchi.ill(random: random, pooOnFloor: tamagotchi.pooOnFloor)
+        let expected = true
+        XCTAssertEqual(tamagotchi.isIll, true)
+    }
+    func testTamagotchiWithHealth6IncreasesHealthBy1AndWeightBy3WhenEatingMeal() {
+        let tamagotchi = Tamagotchi()
+        tamagotchi.health = 6
+        tamagotchi.weight = 0
+        tamagotchi.eatMeal()
+        let expectedHealth = 7
+        let expectedWeight = 3
+        XCTAssertEqual(expectedHealth, tamagotchi.health)
+        XCTAssertEqual(expectedWeight, tamagotchi.weight)
+    }
+    func testTamagotchiWithHealth3IncreasesHealthBy1AndWeightBy1WhenEatingSnack() {
+        let tamagotchi = Tamagotchi()
+        tamagotchi.health = 3
+        tamagotchi.weight = 0
+        tamagotchi.eatSnack()
+        let expectedHealth = 4
+        let expectedWeight = 1
+        XCTAssertEqual(expectedHealth, tamagotchi.health)
+        XCTAssertEqual(expectedWeight, tamagotchi.weight)
+    }
+    func testTamagotchiConsumesMedicineAndBecomesHealthy() {
+        let tamagotchi = Tamagotchi()
+        tamagotchi.isIll = true
+        tamagotchi.consumeMedicine()
+        XCTAssertEqual(tamagotchi.isIll, false)
+    }
+    func testTamagotchiIsNameable() {
+        let tamagotchi = Tamagotchi()
+        tamagotchi.naming(input: "Jeff")
+        XCTAssertEqual(tamagotchi.name, "Jeff")
+    }
+    }
+
