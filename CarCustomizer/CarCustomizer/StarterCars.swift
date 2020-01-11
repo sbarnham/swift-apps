fcd3ce811c37b384addeef8ea2dc279a3f0577d1
sbarnham <barnham.s@etoncollege.org.uk>
2020-01-11 17:31:07 +0000

    added ability to customise cars using 4 different upgrades added funds, which are spent on the upgrades brought total number of cars to 5 changed randomising cars to cycling through cars

---
+++ CarCustomizer/CarCustomizer/StarterCars.swift
@@ -0,0 +1,13 @@
+//
+//  StarterCars.swift
+//  CarCustomizer
+//
+//  Created by Barnham, Samuel (ABH) on 11/01/2020.
+//  Copyright © 2020 Barnham, Samuel (ABH). All rights reserved.
+//
+
+import Foundation
+
+struct StarterCars {
+    var cars = [Car(make: "Mazda", model: "MX-5", topSpeed: 125, handling: 5, acceleration: 7.7), Car(make: "Fiat", model: "500", topSpeed: 105, handling: 8, acceleration: 12.5), Car(make: "Mercedes-Benz", model: "AMG GT R", topSpeed: 145, handling: 4, acceleration: 6.5), Car(make: "Aston Martin", model: "DB-11", topSpeed: 155, handling: 3, acceleration: 7), Car(make: "Ferrari", model: "812", topSpeed: 155, handling: 1, acceleration: 5)]
+}
--- CarCustomizer/CarCustomizer/ViewController.swift
+++ CarCustomizer/CarCustomizer/ViewController.swift
@@ -10,15 +10,117 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    @IBOutlet var aerodynamicEnchancementSwitch: UISwitch!
+    
+    @IBOutlet var turboPackageSwitch: UISwitch!
+    
+    @IBOutlet var remainingFundsDisplay: UILabel!
+    
+    @IBOutlet var engineAndExhaustPackageSwitch: UISwitch!
+    
+    @IBOutlet var tiresPackageSwitch: UISwitch!
+    
     @IBOutlet weak var carStatistics: UILabel!
-    var car = Car(make: "Mazda", model: "MX-5", topSpeed: 125, handling: 5, acceleration: 7.7)
+    
+    var i = 0
+    var remainingFunds = 0 {
+        didSet {
+            remainingFundsDisplay.text = "Remaining Funds: \(remainingFunds)"
+        }
+    }
+    var starterCars = StarterCars()
+    var car: Car? {
+        didSet {
+            carStatistics.text = car?.displayStats()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        carStatistics.text = car.displayStats()
-        
+        car = starterCars.cars[i]
+        remainingFunds = 1000
     }
     
-}
+    @IBAction func cycleCars(_ sender: Any) {
+        i += 1
+        if i == starterCars.cars.count {
+            i = 0
+        }
+        car = starterCars.cars[i]
+        remainingFunds = 1000
+        checkFunds()
+        engineAndExhaustPackageSwitch.isOn = false
+        tiresPackageSwitch.isOn = false
+        turboPackageSwitch.isOn = false
+        aerodynamicEnchancementSwitch.isOn = false
+    }
+    
+    
+    @IBAction func engineAndExhaustToggle(_ sender: Any) {
+        if engineAndExhaustPackageSwitch.isOn {
+            car?.topSpeed += 5
+            remainingFunds -= 400
+        } else {
+            car?.topSpeed -= 5
+            remainingFunds += 400
+        }
+        checkFunds()
+    }
+    
+    @IBAction func tiresToggle(_ sender: Any) {
+        if tiresPackageSwitch.isOn {
+            car?.handling += 2
+            remainingFunds -= 300
+        } else {
+            car?.handling -= 2
+            remainingFunds += 300
+        }
+        checkFunds()
+    }
 
+    @IBAction func turboToggle(_ sender: Any) {
+        if turboPackageSwitch.isOn {
+            car?.acceleration -= 1.0
+            car?.topSpeed -= 3
+            remainingFunds -= 500
+        } else {
+            car?.acceleration += 1.0
+            car?.topSpeed += 3
+            remainingFunds += 500
+        }
+        checkFunds()
+    }
+
+    @IBAction func aerodynamicEnchancementToggle(_ sender: Any) {
+        if aerodynamicEnchancementSwitch.isOn {
+            car?.topSpeed += 15
+            car?.handling -= 1
+            remainingFunds -= 700
+        } else {
+            car?.topSpeed -= 15
+            car?.handling += 1
+            remainingFunds += 700
+        }
+        checkFunds()
+    }
+    
+    func checkFunds() {
+        aerodynamicEnchancementSwitch.isEnabled = true
+        engineAndExhaustPackageSwitch.isEnabled = true
+        turboPackageSwitch.isEnabled = true
+        tiresPackageSwitch.isEnabled = true
+        if remainingFunds < 700 {
+            aerodynamicEnchancementSwitch.isEnabled = false
+        if remainingFunds < 500 {
+            engineAndExhaustPackageSwitch.isEnabled = false
+            turboPackageSwitch.isEnabled = false
+        }
+        if remainingFunds < 300 {
+            tiresPackageSwitch.isEnabled = false
+        }
+    }
+
+}
+}
