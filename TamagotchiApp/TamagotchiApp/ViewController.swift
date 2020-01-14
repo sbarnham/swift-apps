6ec803cfe84765522aea08df4e92a214fbf2be97
sbarnham <barnham.s@etoncollege.org.uk>
2020-01-14 12:25:42 +0000

    begin ting

---
+++ TamagotchiApp/TamagotchiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TamagotchiApp
+//
+//  Created by Barnham, Samuel (ABH) on 14/01/2020.
+//  Copyright © 2020 Barnham, Samuel (ABH). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
